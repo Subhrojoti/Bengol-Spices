e9fed461c2ce7a1afbb0a6be3144a33a2e704315
Subhrojoti <subhrojoti20@gmail.com>
2026-02-04 18:53:55 +0530

    change store schema, for validation with otp, added to two store route replace with the older one

--- BackEnd/routes/store.routes.js
+++ BackEnd/routes/store.routes.js
@@ -1,13 +1,28 @@
 import express from "express";
-import { getMyStores, registerStore } from "../controllers/store.controller.js";
+import {
+  initiateStoreRegistration,
+  verifyStoreOtp,
+  getMyStores,
+} from "../controllers/store.controller.js";
 import { protect } from "../middleware/auth.js";
 import { isAgent } from "../middleware/role.js";
+import { upload } from "../middleware/upload.js";
 
 const router = express.Router();
 
-// 🔒 REGISTER STORE (AGENT ONLY)
-router.post("/register", protect, isAgent, registerStore);
-// GET MY STORES (AGENT)
+// STEP 1 — initiate (send OTP)
+router.post(
+  "/register/initiate",
+  protect,
+  isAgent,
+  upload.single("image"),
+  initiateStoreRegistration,
+);
+
+// STEP 2 — verify OTP
+router.post("/register/verify-otp", protect, isAgent, verifyStoreOtp);
+
+// GET MY STORES
 router.get("/my-stores", protect, isAgent, getMyStores);
 
 export default router;
