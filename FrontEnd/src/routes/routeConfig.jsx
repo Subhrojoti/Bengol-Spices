ce5dde884d1ae1bb689bd322e23a7837f1951ff9
Subhrojoti Nag <141759368+Subhrojoti@users.noreply.github.com>
2026-04-09 15:54:35 +0530

    Refactor routeConfig.jsx imports

--- FrontEnd/src/routes/routeConfig.jsx
+++ FrontEnd/src/routes/routeConfig.jsx
@@ -21,7 +21,6 @@ import PublicRoute from "../routes/PublicRoute";
 import { footerRoutes } from "../config/footerRoutes.js";
 import { mapRoutes } from "../config/mapRoutes.jsx";
 import HomeBase from "../Pages/common/HomeBase.jsx";
-
 import DeliveryProfile from "../components/profile/DeliveryProfile.jsx";
 import ErrorPage from "../components/common/ErrorPage.jsx";
 
